a748b2599fd12df2722501260d0949398edc94f1
Aledvan <del-reklama@yandex.ru>
2024-03-10 07:31:05 +0300

    Squashed commit of the following:
    commit a4a7289cca1e4e0b8a0543d059f2550438c2409f
    Author: Aledvan <del-reklama@yandex.ru>
    Date:   Sun Mar 10 07:23:35 2024 +0300
    
        added new directory
    
    commit 02fb48648c02e7ee1576e94b8a2d4b280bedf5f9
    Author: Aledvan <del-reklama@yandex.ru>
    Date:   Sun Mar 10 06:56:58 2024 +0300
    
        added new files

---
+++ config/DbConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Config;
+
+abstract class DbConfig
+{
+    CONST DB_HOST = "localhost";
+    CONST DB_USER = "tochka-acquiring";
+    CONST DB_PASS = "Jtgw81![A1cdKW8J";
+    CONST DB_NAME = "tochka-acquiring";
+}
---
+++ src/database/Query.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Src\Database;
+
+use Src\Database\Connection;
+
+class Query
+{
+    private $dbh;
+
+    public function __construct()
+    {
+        $this->dbh = Connection::getInstance()->getConnection();
+    }
+
+    public function execute(string $sql, array $params, bool $fetchAll = true)
+    {
+        $stmt = $this->dbh->prepare($sql);
+        foreach ($params as $key => $value) {
+            $stmt->bindValue($key, $value);
+        }
+        $success = $stmt->execute();
+        if (stripos(trim($sql), 'SELECT') === 0) {
+            return $fetchAll ? ($stmt->fetchAll() ?: null) : ($stmt->fetch() ?: null);
+        }
+        return $success;
+    }
+}
